b40fa1c6528ba91d218daadd4907c56c985715fa
AkashMamun <akashmamun222@gmail.com>
2022-11-02 10:33:13 +0600

    my portfolio

---
+++ request.php
@@ -0,0 +1,28 @@
+<?php
+
+if (isset($_REQUEST['n'])) {
+  $user_name = $_REQUEST['n'];
+  $user_email = $_REQUEST['e'];
+  $user_subject = $_REQUEST['s'];
+  $user_message = $_REQUEST['m'];
+
+  $subject = "Message From Portfolio";
+  $to = "akashmamun222@gmail.com";
+
+  $massege = "Name: " . $user_name . "<br>" . "\r\n";
+  $massege .= "Email: " . $user_email . "<br>" . "\r\n";
+  $massege .= "Subject: " . $user_subject . "<br>" . "\r\n";
+  $massege .= "messsage: " . $user_message . "<br>" . "\r\n";
+
+  $headers = "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+  $headers .= "From: Al-Mamun " . "\r\n";
+
+  $mail = mail($to, $subject, $massege, $headers);
+
+  if ($mail == true) {
+    echo "success";
+  } else {
+    echo "error";
+  }
+}
